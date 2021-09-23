957e884ab5e4bc547b801cf5a140f8b392bdfca3
WagnerReis <wagnerreis1997@gmail.com>
2021-09-23 11:20:02 -0300

    Cadastro na aplicação

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -13,6 +13,8 @@ import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import api from '../../services/api';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -56,9 +58,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      // history.push('/');
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login no GoBarber'
+      )
+
+      navigation.goBack();
 
     }catch (err) {
       if (err instanceof Yup.ValidationError) {
